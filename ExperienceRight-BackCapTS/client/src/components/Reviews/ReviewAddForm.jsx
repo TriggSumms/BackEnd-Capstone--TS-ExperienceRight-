cb739057c8aaa90cd803ed46bf7e2d19675de61e
Trigg Summs <stsumms@gmail.com>
2020-10-27 02:10:19 -0500

    tons of issues with the npm react-star-rating, will keep attempting in the morning

--- ExperienceRight-BackCapTS/client/src/components/Reviews/ReviewAddForm.jsx
+++ ExperienceRight-BackCapTS/client/src/components/Reviews/ReviewAddForm.jsx
@@ -3,6 +3,8 @@ import React, { useContext, useState, useEffect } from "react";
 import { ReviewContext } from "../../providers/ReviewProvider";
 import { useHistory, Link } from "react-router-dom";
 import { Button, Form, FormGroup, Label, Input, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import ReactStars from 'react-stars'
+import { render } from 'react-dom'
 
 
 export default function ReviewAddForm() {
@@ -10,22 +12,75 @@ export default function ReviewAddForm() {
 //    const { frequencies, AllFrequencies} = useContext(ReviewContext);
     const { addReview, frequencies, getAllFrequencies } = useContext(ReviewContext);
     const [frequencyId, setFrequencyId] = useState();
+   const [rating, setRating] = useState(``);
 
     const sessionUser = JSON.parse(sessionStorage.getItem("userProfile"));
 
+    const [isLoading, setIsLoading] = useState(false);
 
     const [review, setReview] = useState({
         title: "",
         content: "",
-        rating: 1 ,
+        rating: "",
         dateOfExperience: "",
         frequencyId: "",
         businessId: "",
         userProfileId: sessionUser.id
     });
 
-    const [isLoading, setIsLoading] = useState(false);
+    console.log("Testing Review", review)
+    
 
+    
+    // render(<ReactStars
+    //     count={5}
+    //     onChange={ratingChanged}
+    //     size={24}
+    //     color2={'#ffd700'} />,
+      
+    //     document.getElementById('where-to-render')
+    //   );
+    // const firstExample = {
+    //     size: 30,
+    //     value: 2.5,
+    //     edit: false
+    //   }
+      //reviewrating = parseInt(review.rating);
+
+     const onChange = event => setRating(parseInt(event.target.value));
+
+      const secondExample = {
+        size: 50,
+        count: 10,
+        char: '',
+        color1: '#ff9900',
+        color2: '#6599ff',
+        
+        // onChange={ratingChanged},
+        //onChange={e => setRating(parseInt(e.target.value))}
+         onChange: newValue => {
+        // //     setRating(newValue)
+           console.log(`Example 2: new value is ${newValue}`)}
+         // rating = parseInt(newValue);
+        //{review.rating} = newValue
+      
+      } 
+     console.log("wowtest", setRating)
+     
+    //  const ratingChanged = (newRating) => {
+    //     console.log(newRating)
+    //   }
+
+
+
+
+
+
+    //   const onChange = e => {
+    //     const stateToChange = { ...review };
+    //     stateToChange[e.target.newValue = e.target.review.rating];
+    //     setReview(stateToChange);
+    // };
 
 
 
@@ -40,6 +95,11 @@ export default function ReviewAddForm() {
         stateToChange[e.target.id] = e.target.value;
         setReview(stateToChange);
     };
+    // const handleFieldyChange = e => {
+    //     const stateToChange = { ...review };
+    //     stateToChange[e.target.value] = (parseInt(e.target.value));
+    //     setRating(stateToChange);
+    // };
 
 
     const createNewReview = e => {
@@ -88,6 +148,8 @@ export default function ReviewAddForm() {
                         )}
                         </select>
                         <br />
+
+                        
                         {/* <Label for="imageLocation">Image URL</Label>
                         <Input
                             type="text"
@@ -97,26 +159,7 @@ export default function ReviewAddForm() {
                             placeholder="Url"
                             value={post.imageLocation}
                         /> */}
-                        {/* <Label for="category">Category</Label>
-                        <Input
-                            isOpen={dropdownOpen}
-                            toggle={toggle}
 
-                            required
-                            type="select"
-                            onChange={handleFieldChange}
-                            id="categoryId"
-                            value={post.category}
-                        >
-                            <DropdownToggle caret>
-                                {/* Select Category */}
-                        {/* </DropdownToggle>
-                            <option selected value="default" >Select a Category</option>
-                            {categories.map(category => {
-
-                                return <option key={category.id} value={category.id}>{category.name}</option>
-                            })} */}
-                        {/* </Input> */}
                         <Label for="content">Content</Label>
                         <Input
                             type="text"
@@ -136,15 +179,19 @@ export default function ReviewAddForm() {
                             value={review.dateOfExperience}
                         />
                         <Label for="rating">Rating Of Your Experience</Label>
-                        {/* <Input
-                            type="text"
-                            required
-                            onChange={handleFieldChange}
-                            id="rating"
-
-                            placeholder="rating"
-                            value={parseInt(review.rating)}
-                        /> */}
+                        <ReactStars {...secondExample}   
+                            // type="text"
+                            // required
+                            //  onChange={handleFieldChange}
+                            //  id="rating"
+                            // id="rating" 
+                            //  value= {review.rating}
+                            onChange={onChange} value={review.rating}
+                           //onChange ={handleFieldyChange}
+                            //onChange={e => setRating(parseInt(e.target.value))}
+                            // placeholder="rating"
+                            // value={parseInt(review.rating)}
+                             />  
                         <br />
                         <div>
                             <Button
